ae2d7d4eb7b19ed7ee3eadb04e449dd184f1ba9c
Sergey Smirnov <info@serg-smirnoff.com>
2015-07-26 02:12:02 +0300

    molotok.parser
    parse images from molotok.ru aucro.ua etc

---
+++ urls.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+// all urls to parse and all patches to save images
+
+$urls = array  (
+	"01" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-marki-rossijskoj-imperatorskoj-pochty-124551", "path" => "images/russia/rossiya-do-1917-goda-marki-rossijskoj-imperatorskoj-pochty"),
+        "02" => array("url" => "http://molotok.ru/rossiya-i-sssr-listy-sssr-i-rossiya-124908", "path" => "images/russia/rossiya-i-sssr-listy-sssr-i-rossiya"),
+        "03" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-marki-zemskoj-pochty-124552", "path" => "images/russia/rossiya-do-1917-goda-marki-zemskoj-pochty"),
+        "04" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-marki-carstva-polskogo-124553", "path" => "images/russia/rossiya-do-1917-goda-marki-carstva-polskogo"),
+        "05" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-marki-velikogo-knyazhestva-finlyandskogo-124554", "path" => "images/russia/rossiya-do-1917-goda-marki-velikogo-knyazhestva-finlyandskogo"),
+        "06" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-russkij-levant-124555", "path" => "images/russia/rossiya-do-1917-goda-russkij-levant"),
+        "07" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-russkij-kitaj-124556", "path" => "images/russia/rossiya-do-1917-goda-russkij-kitaj"),
+        "08" => array("url" => "http://molotok.ru/rossiya-do-1917-goda-drugie-marki-rossii-do-1917-goda-124558", "path" => "images/russia/rossiya-do-1917-goda-drugie-marki-rossii-do-1917-goda"),
+        "09" => array("url" => "http://molotok.ru/rossiya-i-sssr-marki-grazhdanskoj-vojny-1917-1923-124545", "path" => "images/russia/rossiya-i-sssr-marki-grazhdanskoj-vojny-1917-1923"),
+        "10" => array("url" => "http://molotok.ru/rsfsr-sssr-rsfsr-1918-1923-124559", "path" => "images/russia/rsfsr-sssr-rsfsr-1918-1923"),
+        "11" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1923-1930-124560", "path" => "images/russia/rsfsr-sssr-sssr-1923-1930"),
+        "12" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1931-1940-124561", "path" => "images/russia/rsfsr-sssr-sssr-1931-1940"),
+        "13" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1941-1950-124562", "path" => "images/russia/rsfsr-sssr-sssr-1941-1950"),
+        "14" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1951-1960-124563", "path" => "images/russia/rsfsr-sssr-sssr-1951-1960"),
+        "15" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1961-1970-124564", "path" => "images/russia/rsfsr-sssr-sssr-1961-1970"),
+        "16" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1971-1980-124565", "path" => "images/russia/rsfsr-sssr-sssr-1971-1980"),
+        "17" => array("url" => "http://molotok.ru/rsfsr-sssr-sssr-1981-1991-124566", "path" => "images/russia/rsfsr-sssr-sssr-1981-1991"),
+        "18" => array("url" => "http://molotok.ru/rossiya-posle-1991-g-rossiya-1992-2000-124567", "path" => "images/russia/rossiya-posle-1991-g-rossiya-1992-2000"),
+        "19" => array("url" => "http://molotok.ru/rossiya-posle-1991-g-rossiya-2001-2010-124568", "path" => "images/russia/rossiya-posle-1991-g-rossiya-2001-2010"),
+        "20" => array("url" => "http://molotok.ru/rossiya-posle-1991-g-rossiya-posle-2011-g-124569", "path" => "images/russia/rossiya-posle-1991-g-rossiya-posle-2011-g"),
+);
+            
+?>
